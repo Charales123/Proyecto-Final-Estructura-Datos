15077a79372169388ec7334bdb33b0d284f54d1f
Yahir Gonzalez Garcia <ryan.yahir106@gmail.com>
2026-06-09 08:07:18 -0600

    feat: version inicial del sistema de reserva

---
+++ PFED.cpp
@@ -0,0 +1,6 @@
+/*              Proyecto Final Estructura de Datos
+============ Generador de Examenes ============ */
+#include <iostream>
+#include <fstream>  //manejo de archivos mas facil
+ using namespace std;
+
