e0c4d9294fcd421e347767ef22bff425d835b3ab
Yahir Gonzalez Garcia <ryan.yahir106@gmail.com>
2026-06-11 10:42:31 -0600

    feat: actualizacion del generador de examenes

--- PFED.cpp
+++ PFED.cpp
@@ -4,3 +4,28 @@
 #include <fstream>  //manejo de archivos mas facil
  using namespace std;
 
+/* ------------ Estructura principal de una pregunta ------------ */
+struct pregunta{
+    string texto;   //texto de la pregunta
+    string op1;
+    string op2;
+    string op3;
+    string op4;
+    string respuesta;   //la respuesta correcta
+    int puntos; //puntos del reactivo
+};
+
+struct nodo{
+    pregunta dato;
+    nodo *anterior;     //al nodo anterior
+    nodo *siguiente;    //al nodo siguiente
+};
+
+struct listaDoble{
+    nodo *cabeza;    //primer nodo
+    nodo* cola;     //ultimo nodo
+    int total;  //total de reactivos
+    listaDoble(): cabeza(nullptr), cola(nullptr), total(0){}    //crea una listaDoble limpia
+};
+
+/* ------------ Prototipos ------------ */
